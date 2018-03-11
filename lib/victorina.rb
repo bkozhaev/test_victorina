d4cf79467e2dc7e891273d4c4d6bee7b90a0b2fd
baur <bkozhaev@gmail.com>
2018-03-11 13:11:23 +0400

    code refactoring

--- lib/victorina.rb
+++ lib/victorina.rb
@@ -1,30 +1,61 @@
 class Victorina
   attr_reader :questions, :right_answers, :bad_answers
 
-  def initialize(questions = [])
-    @questions = questions
+  def initialize
+    @questions = []
+    @right_answers = 0
+    @bad_answers = 0
+    @count = 0
+    @user_input = nil
   end
 
-  def self.from_node(node)
-    questions = []
-    node.each {|q| questions << Question.from_node(q)}
-
-    new(questions)
+  def set_questions(node)
+    node.each do |q|
+      @questions << Question.from_node(q)
+    end
   end
 
-  def to_a
-    @questions
+  def finished?
+    @questions.length == @count
   end
 
-  def check_answer?(variants, answer, questions)
-    variants[answer] == questions.right_variant
+  def ask_questions
+    puts "#{@questions[@count].question} время на вопрос #{questions[@count].time} секунд"
+    @questions[@count].variants.each_with_index do |val, index|
+      puts "#{index + 1}. #{val}"
+    end
+
+    begin
+      Timeout::timeout(questions[@count].time.to_i) do
+        @user_input = gets.chomp
+        unless @user_input.to_i.between?(1, 4)
+          puts "Вводите цифру из диапозона предложенных вариантов"
+          @user_input = gets.chomp
+        end
+      end
+    rescue Timeout::Error
+      @user_input = nil
+    end
+    check_answers
+    @count += 1
   end
 
-  def coun_answers
-    if check_answer?
+  def check_answers
+    answer = @user_input.to_i - 1
+    if @user_input.nil?
+      puts "Время вышло, ответ #{@questions[@count].right_variant}\n\r"
+    elsif @questions[@count].right_variant == @questions[@count].variants[answer]
+      puts "Правильно\n\r"
       @right_answers += 1
     else
+      puts "Неправильно, правильный ответ #{@questions[@count].right_variant}\n\r"
       @bad_answers += 1
     end
+  end
+
+  def show_results
+    puts "Итого правильных ответов: #{@right_answers}"
+    puts "      неправильных ответов: #{@bad_answers}"
+    puts "      неотвеченных ответов: #{@questions.length - (@right_answers + @bad_answers)}"
   end
 end
--- main.rb
+++ main.rb
@@ -11,44 +11,11 @@ abort "Файл #{file_name} не найден" unless File.exists?(file)
 question_nodes = REXML::Document.new(file).root.elements.to_a
 file.close
 
-right_answers = 0
-bad_answers = 0
-user_input = nil
+victorina = Victorina.new
+victorina.set_questions(question_nodes)
 
-victorina = Victorina.from_node(question_nodes)
-
-puts "Мини-викторина. Ответье на вопросы.\n\r"
-victorina.to_a.each do |q|
-  puts "#{q.question} Время на ответ #{q.time} секунд"
-  var_for_present = q.variants.shuffle
-  var_for_present.each_with_index do |variant, index|
-    puts "#{index + 1}. #{variant}"
-  end
-
-  begin
-    Timeout::timeout(q.time.to_i) do
-      user_input = gets.chomp
-      unless user_input.to_i.between?(1, 4)
-        puts "Вводите цифру из диапозона предложенных вариантов"
-        user_input = gets.chomp
-      end
-    end
-  rescue Timeout::Error
-    user_input = nil
-  end
-
-  chosen_index = user_input.to_i - 1
-
-  if user_input.nil?
-    puts "Время вышло, ответ #{q.right_variant}\n\r"
-  elsif victorina.check_answer?(var_for_present, chosen_index, q)
-    puts "Правильно\n\r"
-    right_answers += 1
-  elsif !victorina.check_answer?(var_for_present, chosen_index, q)
-    puts "Неправильно, правильный ответ #{q.right_variant}\n\r"
-    bad_answers += 1
-  end
+until victorina.finished?
+  victorina.ask_questions
 end
 
-puts "Итого: правильных ответов: #{right_answers}"
-puts "       неправильных ответов: #{bad_answers}"
+victorina.show_results
